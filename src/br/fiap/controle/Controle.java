499883c311c8f07b3542ffe5ebb11fb4faf1742f
Antonio Marcos Selmini <marcos.selmini@uol.com.br>
2025-05-15 00:05:23 -0300

    estrutura do projeto

---
+++ src/br/fiap/controle/Controle.java
@@ -0,0 +1,25 @@
+package br.fiap.controle;
+import static java.lang.Long.parseLong;
+import static java.lang.Integer.parseInt;
+import static java.lang.Double.parseDouble;
+import static javax.swing.JOptionPane.*;
+
+public class Controle {
+
+    public void menu() {
+        int opcao;
+        while(true) {
+            opcao = parseInt(showInputDialog(gerarMenu()));
+        }
+    }
+
+    private String gerarMenu() {
+        String aux = "SISTEMA DE COMPRAS ONLINE\n";
+        aux += "1. Adicionar produto\n";
+        aux += "2. Remover produto\n";
+        aux += "3. Fechar compra\n";
+        aux += "4. Finalizar compra\n";
+        return aux;
+    }
+
+}
